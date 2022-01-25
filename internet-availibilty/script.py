977ef185e0fc59892fe0913f974030279ea9e26d
SonnyFishback <sonny.fishback7@gmail.com>
2022-01-25 12:05:36 -0500

    installing selenium

---
+++ internet-availibilty/script.py
@@ -0,0 +1,6 @@
+import selenium
+
+# This is the URL for the internet availibilty checker site. Move to more logical place later.
+internet_checker_URL = 'https://www.t-mobile.com/isp/eligibility'
+
+
